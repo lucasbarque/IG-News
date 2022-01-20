c29caa69fc888745b5ffa5d4c866a1bd94d4294b
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-01-20 19:34:42 -0400

    Autenticação Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,30 @@
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import {FiX} from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
 
-  return isUserLoggedIn ? (
+  const {data: session} = useSession();
+  console.log(session);
+
+
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Lucas Barque
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
+      
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github      
