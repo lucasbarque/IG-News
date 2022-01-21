e9916ec7f4e98529ea011f6f8992a9f395dc67d2
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-01-20 21:46:35 -0400

    Cadastrando customer no stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import {query as q} from 'faunadb';
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -7,5 +10,39 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET            
     }),
-  ],
+  ],    
+  callbacks: {
+    async signIn({ user }) { 
+      const { email } = user;   
+      
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),            
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )          
+          )
+        );
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }      
+    },
+  }
 });
