ef4c871580822da283b3733e10ca39565380c223
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-01-20 22:21:24 -0400

    Validações de customer

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,9 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
+
+
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -8,13 +12,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton( { priceId }: SubscribeButtonProps) {
   const {data: session} = useSession();
   
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if(!session) {
       signIn('github');
       return;
     }
 
-    
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+
+    } catch (error) {
+      alert(error.message);
+    }
     
   }
   return (
