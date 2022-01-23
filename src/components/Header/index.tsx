cbf24059d204351dc24f9f43b318c687fe468c63
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-01-23 11:00:56 -0400

    Active Link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,24 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss'
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts">
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SignInButton/>
