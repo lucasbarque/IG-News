a35502ef62174b6273b60340ab8b5f7579b4dc2a
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-01-23 12:48:13 -0400

    Finalização do projeto

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,9 +1,8 @@
 import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
-
-
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -11,10 +10,16 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton( { priceId }: SubscribeButtonProps) {
   const {data: session} = useSession();
+  const router = useRouter();
   
   async function handleSubscribe() {
     if(!session) {
       signIn('github');
+      return;
+    }
+
+    if(session.activeSubscription) {
+      router.push('/posts');
       return;
     }
 
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -40,13 +40,18 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req });
   const { slug } = params;
 
-  // if(!session) {
-
-  // }
+  if(!session?.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req);
 
-  const response = await prismic.getByUID('publication', String(slug), {});
+  const response = await prismic.getByUID<any>('publication', String(slug), {});
 
   const post = {
     slug,
