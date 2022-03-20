a7fb91ef0f9064a89dfc25bb58e4d26f22a32ca6
Lucas Barque <lucasbarquedasilva@hotmail.com>
2022-03-20 19:13:14 -0400

    Test Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,11 @@ import Image from 'next/image';
 interface HomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   }
 }
 
-export default function Home({ product }: HomeProps) {  
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -23,29 +23,29 @@ export default function Home({ product }: HomeProps) {
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>
-            Get access to all the publications<br/>
+            Get access to all the publications<br />
             <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton priceId={product.priceId}/>
-        </section>       
-        
-        <Image width={336} height={521} src="/images/avatar.svg" alt="Mulher digitando"/>
+          <SubscribeButton />
+        </section>
+
+        <Image width={336} height={521} src="/images/avatar.svg" alt="Mulher digitando" />
       </main>
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async() => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1KK6s1GYv3ECZc0Z3XdCR1Vi');
-  
+
   const product = {
     priceId: price.id,
     amount: new Intl.NumberFormat('pt-BR', {
       style: 'currency',
       currency: 'BRL'
-    }).format(price.unit_amount / 100,),          
+    }).format(price.unit_amount / 100,),
   };
   return {
     props: {
